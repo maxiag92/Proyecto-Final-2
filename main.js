5bd15506f66456810d358a25e8b51319140da687
maxiag92 <140030936+maxiag92@users.noreply.github.com>
2023-10-02 23:38:09 -0300

    detalles

--- main.js
+++ main.js
@@ -1,70 +1,66 @@
 
 function cargarProductos() {
-    fetch('productos.json')    //fetch va y mira lo que hay en el archivo json
-      .then(response => response.json())  //then dice, cuando entiendas lo que hay en el archivo json,hasmelo saber
-      .then(data => {            //una vez se sabe, then dice, hagamos algo con esta info
-        const listaProductos = document.getElementById('productos-lista'); //asigna un lugar en la pagina,para poner productos
+    fetch('productos.json')    
+      .then(response => response.json())  
+      .then(data => {            
+        const listaProductos = document.getElementById('productos-lista'); 
   
-        data.forEach(producto => {    //para cada elemento del archivo json, hagamos.....
-          const listItem = document.createElement('li');  //por cada elemento del jason creamos un li
+        data.forEach(producto => {    
+          const listItem = document.createElement('li');  
           listItem.innerHTML = `                    
             <strong>${producto.nombre}</strong>         
             <p>${producto.descripcion_corta}</p>
             <button class="botonAzul" data-id="${producto.id}">Detalles</button>
                 
-          `;                                                   //aqui se coloca lo que contendra cada li
-          listaProductos.appendChild(listItem);         //esto pega el li en la pagina
+          `;                                                   
+          listaProductos.appendChild(listItem);         
         });
   
-        // Agregar evento de clic a los botones de "Ver Detalles"
-        const botonesVerDetalles = document.querySelectorAll('button');  //esto localiza todos los botones de la pagina
-        botonesVerDetalles.forEach(boton => {                  //a cada boton encontrado, le quiero asignar una tarea
-          boton.addEventListener('click', verDetalle);   // que cuando se le haga click se ejecute "ver detalle"
+        
+        const botonesVerDetalles = document.querySelectorAll('button');  
+        botonesVerDetalles.forEach(boton => {                  
+          boton.addEventListener('click', verDetalle);   
         });
       });
-  }   //aqui termina la funcion cargar productos
+  }   
 
-
-
-  
-  // Función, que al hacer click en el boton, busca el ID del producto relacionado al boton, lo guarda en el LStorage, y diracciona a otra url
-  function verDetalle(event) {   //esta funcion se ejecuta al hacer click en el boton,event registra lo que sucede al hacer click en el boton
-    const idProducto = event.target.getAttribute('data-id'); //esto extrae info especial y unica del boton en cuestion
-    localStorage.setItem('productoSeleccionado', idProducto); //almacenamos esa info especial(ID) del boton en el local storage, que luego se leera desde el archivo detalle.js
-    window.location.href = 'detalle.html';  //le dice al navegador, que vaya a otra pagina
+  function verDetalle(event) {   
+    const idProducto = event.target.getAttribute('data-id'); 
+    localStorage.setItem('productoSeleccionado', idProducto); 
+    window.location.href = 'detalle.html'; 
   }
 
-  cargarProductos();  //esto hace que la pagina cargue automaticamente los productos al cargarse la pagina
+  cargarProductos(); 
 
 
   function mostrarDetalle() {
-    const idProducto = localStorage.getItem('productoSeleccionado');   //esto encuentra el ID del boton que se guardo en el LStorage
-    const detalleProducto = document.getElementById('detalle-producto'); //localiza el elemento html donde se mostrara la informacion
+    const idProducto = localStorage.getItem('productoSeleccionado');   
+    const detalleProducto = document.getElementById('detalle-producto'); 
   
-    if (idProducto) {      //si, hay un ID en Lstorage......
-      fetch('productos.json')  //es el que obtiene los datos de un archivo jason
+    if (idProducto) {     
+      fetch('productos.json')  
         .then(response => response.json())
         .then(data => {
-          const productoSeleccionado = data.find(producto => producto.id === parseInt(idProducto)); //busca dentro del json el producto con ID coincidente
+          const productoSeleccionado = data.find(producto => producto.id === parseInt(idProducto)); 
   
-          if (productoSeleccionado) {      //si se encuentra el producto....con innerhtml ponemos sus detalles en la pagina
+          if (productoSeleccionado) {      
             detalleProducto.innerHTML = `  
               <h2>${productoSeleccionado.nombre}</h2>          
               <p>${productoSeleccionado.descripcion_detallada}</p>
               <img src="imagenes_productos/${productoSeleccionado.imagen}" alt="${productoSeleccionado.nombre}">
               <h4>Precio: ${productoSeleccionado.precio}</h4>
             `;                                              
-          } else {   //sino, mostrar "producto no encontrado"
+          } else {   
             detalleProducto.innerHTML = '<p>Producto no encontrado.</p>';
           }
         });
-    } else {        //si no se encontro un ID en el Lstorage.....
-      detalleProducto.innerHTML = '<p>No se ha seleccionado un producto.</p>';  // mostrar "no se ha seleccionado un producto"
+    } else {        
+      detalleProducto.innerHTML = '<p>No se ha seleccionado un producto.</p>';  
     }
   }
   
-  document.getElementById('volver').addEventListener('click', () => {   //agrega evento que al hacer click en el boton...
-    window.location.href = 'index.html';   //redirigir a la pagina principal
+  document.getElementById('volver').addEventListener('click', () => {  
+    window.location.href = 'index.html';  
   });
   
-  mostrarDetalle();  //para que los detalles del producto se muestren automáticamente cuando se carga la página.
+  mostrarDetalle();  
